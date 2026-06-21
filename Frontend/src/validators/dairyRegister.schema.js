1ed4ac000f0453e9b74bd7e176d9d966e7a23874
vedant-kawale-27 <vedantkawale0304@gmail.com>
2026-06-21 16:55:42 +0530

    Fix missing ifscRegex and upiRegex variables in frontend validators

--- Frontend/src/validators/dairyRegister.schema.js
+++ Frontend/src/validators/dairyRegister.schema.js
@@ -5,6 +5,8 @@ import { z } from "zod";
 const phoneRegex = /^[6-9]\d{9}$/;
 const gstRegex = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[A-Z\d]{1}[Z]{1}[A-Z\d]{1}$/;
 const pincodeRegex = /^[1-9][0-9]{5}$/;
+const ifscRegex = /^[A-Z]{4}0[A-Z0-9]{6}$/;
+const upiRegex = /^[\w.-]+@[\w.-]+$/;
 
 /* ---------- STEP 1 : BRAND ---------- */
 
